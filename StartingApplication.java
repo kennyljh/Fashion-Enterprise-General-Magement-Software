419e0d83c37b9aa1bd861ec6033fa393252d42b8
miharang <miharang@iastate.edu>
2024-11-07 16:07:13 -0600

    starting app

--- StartingApplication.java
+++ StartingApplication.java
@@ -1,10 +1,35 @@
 import Modeling.ModelingDepartment;
 
+import java.util.Scanner;
+
 public class StartingApplication {
-    ModelingDepartment modelingDepartment = new ModelingDepartment();
+    static ModelingDepartment modelingDepartment = new ModelingDepartment();
 
     public static void main(String[] args) {
-        System.out.println("--WELCOME TO FASHION EMPIRE--\n You are logged in as: IT SPECIALIST");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("--WELCOME TO FASHION EMPIRE--\n You are logged in as: IT SPECIALIST\n\nWhich Department would you like to go to?" +
+                "\n1: HR Department" +
+                "\n2: Treasury" +
+                "\n3: Manufacturing" +
+                "\n4: Modeling" +
+                "\n5: Inventory");
+        int choice = s.nextInt();
+
+        switch (choice){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                modelingDepartment.runTasks();
+                break;
+            case 5:
+                break;
+        }
+
     }
 }
 
