d39bfd035bd130bc27394149acaf1acfbfc2340d
Kenny Leong <kennyljh@iastate.edu>
2024-11-13 02:56:19 -0600

    added file retrieval and viewing features

--- src/Treasury/src/interfaces/PayrollInterface.java
+++ src/Treasury/src/interfaces/PayrollInterface.java
@@ -5,14 +5,24 @@ import src.Treasury.src.PayrollApproval;
 public interface PayrollInterface {
 
 	boolean addEmployeeData(String filePath);
+
+	boolean printEmployeeData();
+
+	boolean removeEmployeeByID(String employeeID);
 	
 	boolean addPayrollRates(String filePath);
-	
+
+	boolean printPayrollRates();
+
 	boolean processPayroll(String fileDirectory);
 	
 	void createPayroll(String fileDirectory);
 	
 	void createPayslip(String fileDirectory);
+
+	boolean printPayrollReports();
+
+	boolean printPayslips();
 	
 	PayrollApproval retrieveApproval(String reportID);
 }
