7179c49326c5542dd974b7e49e82f9ba77343d28
Sam <smgumm@iastate.edugit config --global user.email smgumm@iastate.edugit config --global user.email smgumm@iastate.edu>
2024-11-19 20:04:19 -0600

    altered fileStorageHR

--- src/HR/src/fileStorageHR.java
+++ src/HR/src/fileStorageHR.java
@@ -2,10 +2,6 @@ package src.HR.src;
 import src.TextEditor.*;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class fileStorageHR {
@@ -73,24 +69,4 @@ public class fileStorageHR {
             e.printStackTrace();
         }
     }
-
-//    public static void main(String[] args) {
-//        fileStorageHR storageHR = new fileStorageHR();
-//        Map<String, Map<String, String>> data = new LinkedHashMap<>();
-//        Employee employee = new Employee("0", "testdummy", Department.MARKETING, "Senior Design", "Active", 200);
-//        Employee employee1 = new Employee("1", "testdummy", Department.MARKETING, "Senior Design", "Active", 200);
-//        Map<String, String> employeeObject = new LinkedHashMap<>();
-//
-//        employeeObject.put("name", employee.getName());
-//        employeeObject.put("employeeID", employee.getEmployeeID());
-//        employeeObject.put("employeeDepartment", employee.getDepartment());
-//        employeeObject.put("employeePosition", employee.getPosition());
-//        employeeObject.put("employeeStatus", employee.getEmployementStatus());
-//        employeeObject.put("employeeSalary", employee.getSalary());
-//        data.put(employee.getEmployeeID(), employeeObject);
-//        storageHR.poorJarser.setRepositoryStrings(data);
-//        storageHR.poorJarser.writeToTextFile(storageHR.getFilepath() + "\\" + employee.getEmployeeID() + ".txt");
-//        storageHR.deleteFile(storageHR.getFilepath() + "\\" + employee.getEmployeeID() + ".txt");
-//
-//    }
 }
