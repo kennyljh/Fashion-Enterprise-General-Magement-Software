a13e061b0dad36d1520d62d7b1c571b57320eb34
miharang <miharang@iastate.edu>
2024-11-20 21:04:59 -0600

    Add Team Member

--- src/Modeling/src/interfaces/ITeamMember.java
+++ src/Modeling/src/interfaces/ITeamMember.java
@@ -1,8 +1,13 @@
 package src.Modeling.src.interfaces;
 
+import src.HR.src.Employee;
+
 import java.util.Map;
 
 public interface ITeamMember {
     public int getId();
+
+    Employee getEmployeeInfo();
+
     public Map<String, String> toMap();
 }
